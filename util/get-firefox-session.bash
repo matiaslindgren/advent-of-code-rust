658b95139dc0a47a426f890d53bc3328d71ac65d
Matias Lindgren <matias.lindgren@iki.fi>
2021-11-21 22:18:24 +0200

    simplify script

--- util/get-firefox-session.bash
+++ util/get-firefox-session.bash
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 
+function err {
+	echo "$@" >> /dev/stderr
+	exit 1
+}
+
 firefox_dir="$HOME/Library/Application Support/Firefox"
 if [ ! -d "$firefox_dir" ]; then
-	echo "cannot search for firefox cookies in non-existing directory '${firefox_dir}'" >> /dev/stderr
-	exit 1
+	err "cannot search for firefox cookies in non-existing directory '${firefox_dir}'"
 fi
 
 firefox_cookies=$(find "$firefox_dir" -type f -name cookies.sqlite | head -n 1)
 if [ ! -f "$firefox_cookies" ]; then
-	echo "unable to find firefox cookies in '${firefox_dir}'" >> /dev/stderr
-	exit 1
+	err "unable to find firefox cookies in '${firefox_dir}'"
 fi
 
 get_session_value="
@@ -19,9 +22,7 @@ get_session_value="
 "
 token=$(sqlite3 "$firefox_cookies" "$get_session_value")
 if [ -z "$token" ]; then
-	echo unable to find session cookie for host adventofcode.com
-	echo log in with firefox to adventofcode.com, then try again
-	exit 1
+	err 'unable to find session cookie for host adventofcode.com, log in with firefox to adventofcode.com, then try again'
 fi
 outpath="$(dirname $0)/session_token"
 echo $token > "$outpath"
