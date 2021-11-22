aeabe6c5a38490dbac4ea055c0942767dd33c116
Matias Lindgren <matias.lindgren@iki.fi>
2021-11-22 20:15:59 +0200

    simplify util

--- util/src/main.rs
+++ util/src/main.rs
@@ -1,9 +1,21 @@
-mod cli;
 mod input;
 mod output;
 
+use std::env::args;
+
+const USAGE: &str = "usage: aoc year day";
+
+fn parse(nth: usize) -> u32 {
+    let input = args().nth(nth).expect(USAGE);
+    match str::parse::<u32>(&input) {
+        Ok(x) => x,
+        Err(_) => panic!("{}", USAGE),
+    }
+}
+
 fn main() {
-    let (year, day) = cli::parse_args();
+    let year = parse(1);
+    let day = parse(2);
     let input = input::get(year, day);
     let output = output::get(&input, year, day);
     println!("{}", output);
