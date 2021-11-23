7918e1240c28248150d08fcde9824cea139fc295
Matias Lindgren <matias.lindgren@iki.fi>
2021-11-24 00:05:14 +0200

    solve 2019-03

--- aoc/src/common.rs
+++ aoc/src/common.rs
@@ -9,3 +9,21 @@ where
         .filter_map(|x| str::parse::<T>(x).ok())
         .collect()
 }
+
+pub fn directions<Label, Distance>(
+    input: &str,
+    sep: &str,
+    label_len: usize,
+) -> Vec<(Label, Distance)>
+where
+    Label: FromStr,
+    Distance: FromStr,
+{
+    let labels = input
+        .split(sep)
+        .filter_map(|x| str::parse::<Label>(&x[..label_len]).ok());
+    let distances = input
+        .split(sep)
+        .filter_map(|x| str::parse::<Distance>(&x[label_len..]).ok());
+    labels.zip(distances).collect()
+}
--- aoc/src/y2019/d03/mod.rs
+++ aoc/src/y2019/d03/mod.rs
@@ -1,14 +1,50 @@
 use crate::common;
+use std::collections::HashMap;
+
+type WirePaths = HashMap<(i32, i32), (char, u32)>;
 
 pub fn main(input: &str) -> String {
-    let v = common::numbers::<u32>(input, ",");
-    format!("{} {}", find_a(&v), find_b(&v))
+    let (wire1, wire2) = input.split_once('\n').unwrap();
+    let wire1 = common::directions::<char, i32>(wire1, ",", 1);
+    let wire2 = common::directions::<char, i32>(wire2, ",", 1);
+    let mut wire_paths = WirePaths::new();
+    find_paths(&wire1, '1', &mut wire_paths);
+    find_paths(&wire2, '2', &mut wire_paths);
+    let mut a = u32::MAX;
+    let mut b = u32::MAX;
+    for ((y, x), (m, steps)) in wire_paths {
+        if m == 'X' {
+            a = a.min((y.abs() + x.abs()) as u32);
+            b = b.min(steps);
+        }
+    }
+    format!("{} {}", a, b)
 }
 
-fn find_a(_v: &[u32]) -> u32 {
-    0
-}
-
-fn find_b(_v: &[u32]) -> u32 {
-    0
+fn find_paths(wire: &[(char, i32)], mark: char, paths: &mut WirePaths) {
+    let mut y = 0;
+    let mut x = 0;
+    let mut total_steps = 0;
+    for &(direction, steps) in wire {
+        for _ in 0..steps {
+            match direction {
+                'U' => y += 1,
+                'R' => x += 1,
+                'D' => y -= 1,
+                'L' => x -= 1,
+                _ => panic!("unknown direction {:?}", direction),
+            };
+            total_steps += 1;
+            match paths.get(&(y, x)) {
+                Some(&(mark_prev, total_steps_prev)) => {
+                    if mark_prev != 'X' && mark_prev != mark {
+                        paths.insert((y, x), ('X', total_steps + total_steps_prev))
+                    } else {
+                        None
+                    }
+                }
+                None => paths.insert((y, x), (mark, total_steps)),
+            };
+        }
+    }
 }
