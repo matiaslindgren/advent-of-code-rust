93423861ceffad926d1c0e43bdb03b9c2aa80623
Matias Lindgren <matias.lindgren@iki.fi>
2021-11-24 00:18:36 +0200

    simplify 2019-02

--- aoc/src/y2019/d02/mod.rs
+++ aoc/src/y2019/d02/mod.rs
@@ -1,11 +1,11 @@
 use crate::common;
 
 pub fn main(input: &str) -> String {
-    let v = common::numbers::<u32>(input, ",");
+    let v = common::numbers::<usize>(input, ",");
     format!("{} {}", find_a(&mut v.clone(), 12, 2), find_b(&v))
 }
 
-fn find_a(v: &mut [u32], noun: u32, verb: u32) -> u32 {
+fn find_a(v: &mut [usize], noun: usize, verb: usize) -> usize {
     v[1] = noun;
     v[2] = verb;
     let mut i = 0;
@@ -13,13 +13,12 @@ fn find_a(v: &mut [u32], noun: u32, verb: u32) -> u32 {
         let op = v[i];
         match op {
             1 | 2 => {
-                let a = v[v[i + 1] as usize];
-                let b = v[v[i + 2] as usize];
-                let r = &mut v[v[i + 3] as usize];
-                match op {
-                    1 => *r = a + b,
-                    2 => *r = a * b,
-                    _ => (),
+                let a = v[v[i + 1]];
+                let b = v[v[i + 2]];
+                v[v[i + 3]] = match op {
+                    1 => a + b,
+                    2 => a * b,
+                    _ => 0,
                 };
             }
             99 => return v[0],
@@ -29,7 +28,7 @@ fn find_a(v: &mut [u32], noun: u32, verb: u32) -> u32 {
     }
 }
 
-fn find_b(v: &[u32]) -> u32 {
+fn find_b(v: &[usize]) -> usize {
     for noun in 0..100 {
         for verb in 0..100 {
             let res = find_a(&mut v.to_vec(), noun, verb);
