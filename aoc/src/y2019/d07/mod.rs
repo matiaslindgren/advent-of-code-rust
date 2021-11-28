a00d3992b33863152fe1208fbac7fb644a63d1c4
Matias Lindgren <matias.lindgren@iki.fi>
2021-11-28 13:29:32 +0200

    solve 2019-07

--- aoc/src/y2019/d07/mod.rs
+++ aoc/src/y2019/d07/mod.rs
@@ -1,16 +1,42 @@
-use crate::common;
+use crate::y2019::intcode;
+use itertools::Itertools;
 
-pub fn main(input: &str) -> String {
-    let v = common::numbers::<u32>(input, ",");
-    let a = find_a(&v);
-    let b = find_b(&v);
+pub fn main(prog: &str) -> String {
+    let a = find_a(prog);
+    let b = find_b(prog);
     format!("{} {}", a, b)
 }
 
-fn find_a(_v: &[u32]) -> u32 {
-    0
+fn find_a(prog: &str) -> i64 {
+    (0..=4)
+        .permutations(5)
+        .unique()
+        .into_iter()
+        .fold(0, |max_signal, phases| {
+            let out = phases
+                .iter()
+                .fold(0, |signal, &phase| intcode::run(prog, &[phase, signal]));
+            max_signal.max(out)
+        })
 }
 
-fn find_b(_v: &[u32]) -> u32 {
-    0
+fn find_b(prog: &str) -> i64 {
+    (5..=9)
+        .permutations(5)
+        .unique()
+        .into_iter()
+        .fold(0, |max_signal, phases| {
+            let mut amps = vec![intcode::IntCode::new(prog); 5];
+            for (amp, phase) in (amps.iter_mut()).zip(phases) {
+                amp.push_input(phase);
+            }
+            let mut signal = 0;
+            while !amps.iter().all(|a| a.terminated) {
+                for amp in amps.iter_mut() {
+                    amp.push_input(signal);
+                    signal = signal.max(amp.run());
+                }
+            }
+            max_signal.max(signal)
+        })
 }
