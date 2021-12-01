06723886e4dca62fbf9ca70f4fd4a0ba9a321d69
Matias Lindgren <matias.lindgren@iki.fi>
2021-12-01 08:32:07 +0200

    simplify 2021-1

--- aoc/src/y2021/d01/mod.rs
+++ aoc/src/y2021/d01/mod.rs
@@ -8,17 +8,10 @@ pub fn main(input: &str) -> String {
 }
 
 fn find_a(v: &[i32]) -> i32 {
-    v.iter()
-        .zip(v[1..].iter())
-        .fold(0, |n, (x0, x1)| n + (x0 < x1) as i32)
+    v.windows(2).map(|w| (w[0] < w[1]) as i32).sum()
 }
 
 fn find_b(v: &[i32]) -> i32 {
-    let mut s = v[0] + v[1] + v[2];
-    let mut sums: Vec<i32> = vec![s];
-    sums.extend(v.iter().zip(v[3..].iter()).map(|(x0, x3)| {
-        s += -x0 + x3;
-        s
-    }));
+    let sums: Vec<i32> = v.windows(3).map(|w| w.iter().sum()).collect();
     find_a(&sums)
 }
