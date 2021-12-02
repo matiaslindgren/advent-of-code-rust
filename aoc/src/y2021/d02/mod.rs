6f1ec7fe489a222aee70df0cbf76eb773f536f67
Matias Lindgren <matias.lindgren@iki.fi>
2021-12-02 07:09:36 +0200

    solve 2021-02

--- aoc/src/y2021/d02/mod.rs
+++ aoc/src/y2021/d02/mod.rs
@@ -1,8 +1,40 @@
 use crate::common;
 
 pub fn main(input: &str) -> String {
-    let _v = common::items::<i32>(input, "\n");
-    let a = 0;
-    let b = 0;
+    let v = common::pairs::<String, i32>(input, " ");
+    let a = find_a(&v);
+    let b = find_b(&v);
     format!("{} {}", a, b)
+}
+
+fn find_a(v: &[(String, i32)]) -> i32 {
+    let mut h = 0;
+    let mut d = 0;
+    for (op, x) in v {
+        match op.as_ref() {
+            "forward" => h += x,
+            "down" => d += x,
+            "up" => d -= x,
+            _ => (),
+        }
+    }
+    d * h
+}
+
+fn find_b(v: &[(String, i32)]) -> i32 {
+    let mut h = 0;
+    let mut d = 0;
+    let mut a = 0;
+    for (op, x) in v {
+        match op.as_ref() {
+            "forward" => {
+                h += x;
+                d += a * x;
+            }
+            "down" => a += x,
+            "up" => a -= x,
+            _ => (),
+        }
+    }
+    d * h
 }
