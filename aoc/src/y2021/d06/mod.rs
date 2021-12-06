10ad40882668e278350ad50f33865c4097286940
Matias Lindgren <matias.lindgren@iki.fi>
2021-12-06 09:36:27 +0200

    solve 2021-06

--- aoc/src/y2021/d06/mod.rs
+++ aoc/src/y2021/d06/mod.rs
@@ -1,8 +1,35 @@
 use crate::common;
+use std::collections::HashMap;
 
 pub fn main(input: &str) -> String {
-    let _v = common::items::<i32>(input, "\n");
-    let a = 0;
-    let b = 0;
+    let fish = common::items::<i32>(input, ",");
+    let mut fc = FishCounter::default();
+    let a: i64 = fish.iter().map(|&t| fc.fish_count(79, t)).sum();
+    let b: i64 = fish.iter().map(|&t| fc.fish_count(255, t)).sum();
     format!("{} {}", a, b)
+}
+
+#[derive(Default)]
+struct FishCounter {
+    cache: HashMap<(i32, i32), i64>,
+}
+
+impl FishCounter {
+    fn fish_count(&mut self, days: i32, timer: i32) -> i64 {
+        match self.cache.get(&(days, timer)) {
+            Some(count) => *count,
+            None => {
+                let count = {
+                    if days < timer {
+                        1
+                    } else {
+                        self.fish_count(days - timer, 7)
+                            + self.fish_count(days - timer, 9)
+                    }
+                };
+                self.cache.insert((days, timer), count);
+                count
+            }
+        }
+    }
 }
