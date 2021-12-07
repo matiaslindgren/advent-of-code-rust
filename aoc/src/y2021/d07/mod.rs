3dabd02259e4315ab4a8a7e1e83e30f89f7bc98b
Matias Lindgren <matias.lindgren@iki.fi>
2021-12-07 21:34:24 +0200

    simplify 2021-07

--- aoc/src/y2021/d07/mod.rs
+++ aoc/src/y2021/d07/mod.rs
@@ -5,15 +5,10 @@ pub fn main(input: &str) -> String {
     let min_pos = *crabs.iter().min().unwrap();
     let max_pos = *crabs.iter().max().unwrap();
     let (a, b) =
-        (min_pos..max_pos).fold((i32::MAX, i32::MAX), |(a, b), pos| {
-            let a_delta = crabs.iter().map(|crab| (crab - pos).abs()).sum();
-            let b_delta = crabs
-                .iter()
-                .map(|crab| {
-                    let n = (crab - pos).abs();
-                    n * (n + 1) / 2
-                })
-                .sum();
+        (min_pos..=max_pos).fold((i32::MAX, i32::MAX), |(a, b), pos| {
+            let deltas = crabs.iter().map(|crab| (crab - pos).abs());
+            let a_delta = deltas.clone().sum();
+            let b_delta = deltas.map(|d| d * (d + 1) / 2).sum();
             (a.min(a_delta), b.min(b_delta))
         });
     format!("{} {}", a, b)
