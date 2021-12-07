b1c25272f5726f5dd697e95ed1325bd3812fb461
Matias Lindgren <matias.lindgren@iki.fi>
2021-12-07 07:41:08 +0200

    solve 2021-07

--- aoc/src/y2021/d07/mod.rs
+++ aoc/src/y2021/d07/mod.rs
@@ -1,8 +1,20 @@
 use crate::common;
 
 pub fn main(input: &str) -> String {
-    let _v = common::items::<i32>(input, "\n");
-    let a = 0;
-    let b = 0;
+    let crabs = common::items::<i32>(input, ",");
+    let min_pos = *crabs.iter().min().unwrap();
+    let max_pos = *crabs.iter().max().unwrap();
+    let (a, b) =
+        (min_pos..max_pos).fold((i32::MAX, i32::MAX), |(a, b), pos| {
+            let a_delta = crabs.iter().map(|crab| (crab - pos).abs()).sum();
+            let b_delta = crabs
+                .iter()
+                .map(|crab| {
+                    let n = (crab - pos).abs();
+                    n * (n + 1) / 2
+                })
+                .sum();
+            (a.min(a_delta), b.min(b_delta))
+        });
     format!("{} {}", a, b)
 }
